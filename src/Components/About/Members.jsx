1c822d49dcf3c4c1b8ba02fe2ac06d65f1b0082d
Botir Akhmedov <95535127+botirpyjs@users.noreply.github.com>
2022-07-14 18:45:14 +0500

    Botir

--- src/Components/About/Members.jsx
+++ src/Components/About/Members.jsx
@@ -3,37 +3,43 @@ import Facebook from '@mui/icons-material/FacebookSharp';
 import Twitter from '@mui/icons-material/Twitter';
 import LinkedIn from '@mui/icons-material/LinkedIn'
 
+import designer from '../img/team/bulk/img1.png'
+import frontend1 from '../img/team/bulk/img2.png'
+import frontend2 from '../img/team/bulk/img6.png'
+import backend1 from '../img/team/bulk/img3.png'
+import backend2 from '../img/team/bulk/img4.png'
+import backend3 from '../img/team/bulk/img5.jpg'
 export default function Member() {
     const bios = [
         {
             name: 'a. nomonov',
-            img: require('../img/img1.png'),
+            img: designer,
             job: 'UI/ux designer',
         },
         {
             name: 'a. arabboyev',
-            img: require('../img/img2.png'),
+            img: frontend2,
             job: 'frontend developer'
         },
         {
             name: 'm. Abdullayev',
-            img: require('../img/img1.png'),
+            img: backend3,
             job: 'backend developer',
         },
         {
             name: 'b. akhmedov',
-            img: require('../img/img4.png'),
+            img: frontend1,
             job: 'frontend developer'
         }
         ,
         {
             name: 'a. abdulhamidov',
-            img: require('../img/img5.png'),
+            img: backend1,
             job: 'backend developer',
         },
         {
             name: 'u. qobiljonov',
-            img: require('../img/img2.png'),
+            img: backend2,
             job: 'backend developer'
         },
         {
